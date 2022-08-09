6af41987d632ad08422cdf6b8a97f488be08fcb5
LINO ADRIÁN PÁEZ REYES <54821048+Adriianpr919@users.noreply.github.com>
2022-08-09 12:57:43 -0500

    varios cambios.
    @Adriianpr919

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+      <p>
+        Inside App Component
+      </p>
+    </div>
+);
+
+export default App;
