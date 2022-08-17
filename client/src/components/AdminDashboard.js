bb6a44caf39c2a55b81e7c2be03fec43ff602397
LINO ADRIÁN PÁEZ REYES <54821048+Adriianpr919@users.noreply.github.com>
2022-08-17 18:03:46 -0500

    varios cambios.
    @Adriianpr919

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,7 +1,78 @@
 import React from 'react';
 
 const AdminDashboard = () => {
-    return <div>AdminDashboard</div>;
-}
+    /********************************************** 
+     * VIEWS
+    **********************************************/
+    const showHeader = () => (
+        <div className="bg-dark text-white py-4">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>
+                            <i class="fa-solid fa-grid-horizontal"> Panel De Administrador.</i>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+    const showActionBtns = () => (
+        <div className="bg-light my-2">
+            <div className="container">
+                <div className="row pb-3">
+                    <div className="col-md-4 my-1">
+                        <button className="btn btn-outline-dark btn-block">
+                            <i class="fa-solid fa-circle-plus"> Añadir Categoría.</i>
+                        </button>
+                    </div>
+
+                    <div className="col-md-4 my-1">
+                        <button className="btn btn-outline-info btn-block">
+                            <i class="fa-solid fa-circle-plus"> Añadir Categoría Talla.</i>
+                        </button>
+                    </div>
+
+                    <div className="col-md-4 my-1">
+                        <button className="btn btn-outline-warning btn-block">
+                            <i class="fa-solid fa-circle-plus"> Añadir Categoría Color De Oro.</i>
+                        </button>
+                    </div>
+
+                    <div className="col-md-4 my-1">
+                        <button className="btn btn-outline-secondary btn-block">
+                            <i class="fa-solid fa-circle-plus"> Añadir Categoría Color De Piedra.</i>
+                        </button>
+                    </div>
+
+                    <div className="col-md-4 my-1">
+                        <button className="btn btn-outline-primary btn-block">
+                            <i class="fa-solid fa-circle-plus"> Añadir Productos.</i>
+                        </button>
+                    </div>
+
+                    <div className="col-md-4 my-1">
+                        <button className="btn btn-outline-success btn-block">
+                            <i class="fa-solid fa-eye"> 
+                                {' '}
+                                Ver Pedidos.
+                            </i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+    /********************************************** 
+     * RENDERING
+    **********************************************/
+    return (
+        <section>
+            { showHeader() }
+            { showActionBtns() }
+        </section>
+    );
+};
 
 export default AdminDashboard;
